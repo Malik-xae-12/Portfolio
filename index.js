11e00f0ecb0b71dde74d9c1728e31c339945231d
Malik-xae-12 <141534004+Malik-xae-12@users.noreply.github.com>
2023-11-15 17:34:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Setup and start animation! 
+
+
+  var typed = new Typed("#element", {
+    strings: [
+      "Web Developer",
+      "Data Analyst",
+      "Vedio Editor",
+      "Web Designer",
+    ],
+    typeSpeed: 60,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true,
+  });
+
+  
